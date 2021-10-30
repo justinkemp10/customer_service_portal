e26dd944fc92d0da27ed70c10db82073aa9b09de
Edgar Ortega <edort93@gmail.com>
2021-10-30 12:04:45 -0500

    added seed data

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Ticket } = require('../models');
+
+const userData = require('./userData.json');
+const TicketData = require('./TicketData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const ticket of projectData) {
+    await Ticket.create({
+      ...ticket,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
