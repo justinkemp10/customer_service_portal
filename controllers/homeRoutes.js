e6efe7212d621c1fc92ae820d42430265a3622d1
Edgar Ortega <edort93@gmail.com>
2021-11-01 22:20:24 -0500

    replaced project with ticket

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
+      tickets, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
